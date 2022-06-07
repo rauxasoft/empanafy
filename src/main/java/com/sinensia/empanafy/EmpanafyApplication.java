ef2170ac2ea5ee3b0391a5d0d89077ea62241628
Jordi <jordi@gmail.com>
2022-06-07 20:48:56 +0200

    commit inicial

---
+++ src/main/java/com/sinensia/empanafy/EmpanafyApplication.java
@@ -0,0 +1,13 @@
+package com.sinensia.empanafy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpanafyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpanafyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sinensia/empanafy/EmpanafyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinensia.empanafy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpanafyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
