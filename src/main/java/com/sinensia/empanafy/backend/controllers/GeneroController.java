c8c50204e9285f70e84791f375096e65af89a945
Jordi <jordi@gmail.com>
2022-06-09 21:20:28 +0200

    se crea implementacion DUMMY de GeneroServicesImpl

--- src/main/java/com/sinensia/empanafy/backend/controllers/GeneroController.java
+++ src/main/java/com/sinensia/empanafy/backend/controllers/GeneroController.java
@@ -1,5 +1,36 @@
 package com.sinensia.empanafy.backend.controllers;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sinensia.empanafy.backend.model.Genero;
+import com.sinensia.empanafy.backend.services.GeneroServices;
+import com.sinensia.empanafy.backend.services.impl.GeneroServicesImpl;
+
+@RestController
 public class GeneroController {
 
+	private GeneroServices generoServices = new GeneroServicesImpl();
+	
+	@GetMapping("/generos")
+	public List<Genero> getAll(HttpServletRequest request){
+		System.out.println(request.getRemoteAddr());
+		return generoServices.getAll();
+	}
+	
+	@GetMapping("/generos/{codigo}")
+	public Genero getByCodigo(@PathVariable Integer codigo, HttpServletRequest request) {
+		System.out.println(request.getRemoteAddr() + " ---> " + codigo);
+		
+		return generoServices.read(codigo);
+	}
+	
+	
+	// http://10.250.18.3:8080/generos
+	// http://10.250.18.3:8080/generos/100
 }
--- src/main/java/com/sinensia/empanafy/backend/services/GeneroServices.java
+++ src/main/java/com/sinensia/empanafy/backend/services/GeneroServices.java
@@ -1,5 +1,14 @@
 package com.sinensia.empanafy.backend.services;
 
+import java.util.List;
+
+import com.sinensia.empanafy.backend.model.Genero;
+
 public interface GeneroServices {
 
+	void create(Genero genero);
+	Genero read(int codigo);
+	
+	List<Genero> getAll();
+	
 }
