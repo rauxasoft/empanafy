b192f29aa1f96db6eb9766eae57014ea1ebc9375
Jordi <jordi@gmail.com>
2022-07-20 18:23:23 +0200

    nuevos requisitos para jornada de miercoles 20

---
+++ nuevos_requisitos.java
@@ -0,0 +1,32 @@
+Nuevos requisitos en proyecto empanafy
+======================================
+
+
+Canción
+=======
+
+1.- Se añade instrumental (boolean)
+2.- Se añade puntuación (int) 
+3.- Se añade comentario (String)
+
+Compositor
+==========
+
+1.- Se añade fecha de nacimiento (puede ser null)
+2.- Se añade fecha de defunción (puede ser null)
+3.- Se separa el nombre en dos campos: nombre y apellidos
+
+Interprete
+==========
+
+1.- Se añade fecha de nacimiento (puede ser null)
+2.- Se añade fecha de defunción (puede ser null)
+3.- Se separa el nombre en dos campos: nombre y apellidos
+
+Retos opcionales en las vistas (JSP)
+====================================
+
+1.- Hacer que el tiempo de la canción se muestre en formato 4' 22"
+2.- Hacer que todas las fechas se muestren formateadas como dd/MM/yyyy
+3.- Hacer que los campos numéricos queden alineados a la derecha
+
